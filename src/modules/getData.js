5f6306efda02b9bc14406a411c850f2386818aa6
belaymit <2belamit@gmail.com>
2022-12-20 14:36:29 +0300

    added a module that fetches data from API

---
+++ src/modules/getData.js
@@ -0,0 +1,12 @@
+import renderScore from './renderScore.js';
+
+const getData = async () => {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/1222/scores', {
+    method: 'GET',
+  });
+  const data = response.json();
+  console.log(data);
+  data.then((object) => renderScore(object.result));
+};
+
+export default getData;
